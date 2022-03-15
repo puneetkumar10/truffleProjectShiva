75e211a2020fe3e788fac7296ba94fde2888f25c
puneetkumar10 <puneet25june@gmail.com>
2022-03-15 19:50:51 +0530

    first commit

---
+++ test/Erc20.test.js
@@ -0,0 +1,9 @@
+// const Erc20 = artifacts.require("Erc20");
+
+// contract("Erc20", (accounts) => {
+//     BeforeUnloadEvent(async () => {
+//         instance = await Erc20.deployed()
+//     })
+
+//     it('ensures that the starting balance is ')
+// })
